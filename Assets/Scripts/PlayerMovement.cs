37ff94069c1627bb4f7f108e1366d54c9d2d9397
MDKTU <78430321+MDKTU@users.noreply.github.com>
2021-02-14 23:45:07 +0200

    added ground spawning/deleting, player jumping

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,18 +14,26 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody rb;
 
     float horizontalInput;
+    float verticalInput;
+
     public float horizontalMultiplier = 2;
+    public float verticalMultiplier = 2;
 
     private void FixedUpdate()
     {
         Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
         Vector3 horizontalMove = transform.right * horizontalInput * speed * Time.fixedDeltaTime * horizontalMultiplier;
         rb.MovePosition(rb.position + forwardMove + horizontalMove);
+
+        Vector3 verticalMove = transform.up * verticalInput * speed * Time.fixedDeltaTime * verticalMultiplier;
+        rb.MovePosition(rb.position + forwardMove + verticalMove);
+
     }
 
     // Update is called once per frame
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
     }
 }
